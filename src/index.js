9243372fcc433d3045199a70e60485ac691e72a2
AnirudhMathur12 <anirudhmathur12@gmail.com>
2023-03-29 00:33:49 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 const PORT = 3000;
 
 app.get('/', (req, res) => {
@@ -11,8 +15,9 @@ app.post('/', (req, res) => {
     res.send('Moo moo ha');
 });
 
-app.post('/', (req, res) => {
+app.post('/api/customers', (req, res) => {
     console.log(req.body);
+    res.send(req.body);
 });
 
 app.listen(PORT, () => {
